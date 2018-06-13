9e356abc93d63f0622767cf73f7429aff7c65804
gered <gered@blarg.ca>
2018-06-13 19:47:16 -0400

    version 1.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mtgcoll "1.2.1-SNAPSHOT"
+(defproject mtgcoll "1.2.1"
   :description  "Magic: The Gathering card database and personal collection management web app."
   :url          "https://github.com/gered/mtgcoll"
   :license      {:name "MIT License"
